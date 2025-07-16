48202fca9a00cfc302b7765a537a3dab0d060bc4
Derly Jordan Aguirre <34700662+der1y@users.noreply.github.com>
2025-07-16 15:56:58 -0400

    Time: 31 ms (59.31%), Space: 67.7 MB (42.18%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -5,22 +5,21 @@ class Solution {
             numSet.add(num);
         }
 
-        int maxLength = 0;
+        int maxCount = 0;
 
         for (int num : numSet) {
-            if (!numSet.contains(num - 1)) {
+            if(!numSet.contains(num - 1)) {
                 int currentNum = num;
-                int currentLength = 1;
-                
-                while(numSet.contains(currentNum + 1)) {
+                int currentCount = 1;
+
+                while (numSet.contains(currentNum + 1)) {
                     currentNum++;
-                    currentLength++;
+                    currentCount++;
                 }
 
-                maxLength = Math.max(maxLength, currentLength);
+                maxCount = Math.max(maxCount, currentCount);
             }
         }
-        
-        return maxLength;
+        return maxCount;
     }
 }
