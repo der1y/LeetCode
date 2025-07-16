83d56ecf7fe672758d896b8e623040d251d17c74
Derly Jordan Aguirre <34700662+der1y@users.noreply.github.com>
2025-07-16 16:58:31 -0400

    Time: 0 ms (100%), Space: 44.9 MB (78.87%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,19 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int pointer = 1;
-        int[] indices = new int[2];
-
-        for (int i = 0; i < nums.length; i++) {
-            for (int x = pointer; x < nums.length; x++) {
-                if (nums[i] + nums[x] == target) {
-                    indices[0] = i;
-                    indices[1] = x;
-                    break;
-                    // return indices;
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            for (int k = i; k < n; k++) {
+                if (nums[k - i] + nums[k] == target) {
+                    return new int[]{k - i, k};
                 }
             }
-            pointer++;
         }
-        return indices;
+        return null;
     }
 }
