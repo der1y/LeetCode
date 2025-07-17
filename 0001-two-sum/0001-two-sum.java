7470690555740080924afe673d160c46d9fcfbea
Derly Jordan Aguirre <34700662+der1y@users.noreply.github.com>
2025-07-17 11:33:57 -0400

    Time: 0 ms (100%), Space: 44.9 MB (78.87%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
+
         for (int i = 1; i < n; i++) {
             for (int k = i; k < n; k++) {
                 if (nums[k - i] + nums[k] == target) {
